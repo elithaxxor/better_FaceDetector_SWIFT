27fcae1c6b059f951f0ac4724763ea7bdcf00351
“elithaxxor” <aldel.alaali@icloud.com>
2022-05-02 21:13:55 -0700

    Initial Commit

---
+++ betterFaceDetector/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  betterFaceDetector
+//
+//  Created by a-robota on 5/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
